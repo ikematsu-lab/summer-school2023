4bfa1288388dc4bbc7a35590ab5db4e3fbbb68fb
Yokohide0317 <yokohide0317@gmail.com>
2024-06-26 09:40:48 +0900

    handoutの場所変更

--- scripts/handouts.sh
+++ scripts/handouts.sh
@@ -4,6 +4,6 @@
 #user="${1}"
 
 while read user; do
-  cp /opt/notebooks/sequences.fasta /home/${user}/
-  chown ${user}: /home/${user}/sequences.fasta
+  cp /opt/notebooks/sequences.fasta /home/${user}/notebook/
+  chown ${user}: /home/${user}/notebook/sequences.fasta
 done < /tmp/users.txt
