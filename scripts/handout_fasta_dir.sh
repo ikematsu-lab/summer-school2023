838df46059e80a78710d2e5265abbeccae138126
Yokohide0317 <yokohide0317@gmail.com>
2024-08-09 16:51:53 +0900

    2024

--- scripts/handout_fasta_dir.sh
+++ scripts/handout_fasta_dir.sh
@@ -4,7 +4,7 @@
 #user="${1}"
 
 while read user; do
-  cp /opt/notebooks/list /home/${user}/list
+  cp -r /opt/notebooks/list /home/${user}/list
   cp /opt/notebooks/essential.fasta /home/${user}/essential.fasta
   chown ${user}: /home/${user}/essential.fasta
   chown -R ${user}: /home/${user}/list
